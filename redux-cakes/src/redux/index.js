5e274d99c3b39844df3f31feaf54ebca7d8cdaf2
Dan Fellner <dfellner92@gmail.com>
2021-12-05 20:20:26 -0500

    user reducer logic

--- redux-cakes/src/redux/index.js
+++ redux-cakes/src/redux/index.js
@@ -1,3 +1,4 @@
 export { buyCake, restockCake } from './cakes/cakeActions';
 export { buyIceCream, restockIceCream } from './iceCream/iceCreamActions';
 export { colorRed, colorGreen } from './colors/colorActions';
+export * from './user/userActions';
--- redux-cakes/src/redux/store.js
+++ redux-cakes/src/redux/store.js
@@ -4,11 +4,13 @@ import logger from 'redux-logger';
 import cakeReducer from "./cakes/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
 import colorReducer from './colors/colorReducer';
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   cake: cakeReducer,
   iceCream: iceCreamReducer,
-  colors: colorReducer
+  colors: colorReducer,
+  users: userReducer
 });
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
